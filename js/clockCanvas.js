7700c8ebbfc143bf3a364a17bbc12a0e65f7e014
yanglinxiao <511500064@qq.com>
2016-11-22 00:01:53 +0800

    添加上小球滚落效果的计时器

--- js/clockCanvas.js
+++ js/clockCanvas.js
@@ -1,10 +1,10 @@
 var canvas = document.getElementById('canvas'),
 	context = canvas.getContext('2d'),
-	endTime = new Date(2016,10,21,23,40,00),//截至时间
+	endTime = new Date(2016,10,22,23,50,00),//截至时间
 	currentTimeSeconds = getCurrentTimeSeconds(),//当前剩余秒数
 	balls = [];
-const CANVAS_WIDTH = 1024,
-	CANVAS_HEIGHT = 768,
+const CANVAS_WIDTH = 1080,
+	CANVAS_HEIGHT = 640,
 	MARGIN_LEFT = 10,//数字之间的左边距
 	RADIUS = 8;	//绘制小圆点的半径
 
@@ -38,22 +38,81 @@ function shapeData(){
 	paintShape(32*2*(RADIUS+1)+MARGIN_LEFT,0,10,context);
 	paintShape(36*2*(RADIUS+1)+MARGIN_LEFT,0,parseInt(seconds/10),context);
 	paintShape(44*2*(RADIUS+1)+MARGIN_LEFT,0,parseInt(seconds%10),context);
+
+	for(var i = 0; i < balls.length; i++){
+		context.fillStyle = balls[i].color;
+		context.beginPath();
+		context.arc(balls[i].x,balls[i].y,RADIUS,0,2*Math.PI,false);
+		context.fill();
+		context.closePath();
+		context.stroke();
+	}
 }
 
 /*更新剩余时间的数据*/
 function update(){
 	var nextTimeSeconds = getCurrentTimeSeconds();
-	var nextHours = parseInt(nextSeconds/3600);
-	var nextMinutes = parseInt((nextSeconds-nextHours*3600)/60);
-	var nextSeconds = nextSeconds % 60;
+	var nextHours = parseInt(nextTimeSeconds/3600);
+	var nextMinutes = parseInt((nextTimeSeconds-nextHours*3600)/60);
+	var nextSeconds = nextTimeSeconds % 60;
 	var currentHours = parseInt(currentTimeSeconds/3600);
 	var currentMinutes = parseInt((currentTimeSeconds-currentHours*3600)/60);
 	var currentSeconds = currentTimeSeconds % 60;
 	if(currentSeconds != nextSeconds){
+		if(parseInt(currentHours/10)!=parseInt(nextHours/10)){
+			addBalls(0,0,parseInt(nextHours/10));
+		}
+		if(parseInt(currentHours%10)!=parseInt(nextHours%10)){
+			addBalls(7*2*(RADIUS+1)+MARGIN_LEFT,0,parseInt(nextHours%10));	
+		}
+		if(parseInt(currentMinutes/10)!=parseInt(nextMinutes/10)){
+			addBalls(18*2*(RADIUS+1)+MARGIN_LEFT,0,parseInt(nextMinutes/10));
+		}
+		if(parseInt(currentMinutes%10)!=parseInt(nextMinutes%10)){
+			addBalls(25*2*(RADIUS+1)+MARGIN_LEFT,0,parseInt(nextMinutes%10));
+		}
+		if(parseInt(currentSeconds/10)!=parseInt(nextSeconds/10)){
+			addBalls(36*2*(RADIUS+1)+MARGIN_LEFT,0,parseInt(nextSeconds/10));
+		}
+		if(parseInt(currentSeconds%10)!=parseInt(nextSeconds%10)){
+			addBalls(44*2*(RADIUS+1)+MARGIN_LEFT,0,parseInt(nextSeconds%10));
+		}
 		currentTimeSeconds = nextTimeSeconds;
+		
+	}
+	updateBalls();
+}
+
+function updateBalls() {
+	for(var i = 0; i < balls.length; i++){
+		balls[i].x += balls[i].Vx;
+		balls[i].y += balls[i].Vy;
+		balls[i].Vy += balls[i].g;
+		if(balls[i].y > (CANVAS_HEIGHT - RADIUS)){
+			balls[i].y = CANVAS_HEIGHT - RADIUS;
+			balls[i].Vy = - balls[i].Vy * 0.75;
+		}
 	}
 }
 
+/*改变数字的，就添加小球进行滚落,也就是绘制改变的数字的图形*/
+function addBalls (x,y,num) {
+	for(var i = 0;i < digit[num].length; i++){
+		for(var j = 0;j < digit[num][i].length;j++){
+			if(digit[num][i][j]==1){
+				var ball = {
+					x:x+j*2*(RADIUS+1)+(RADIUS+1),
+					y:y+i*2*(RADIUS+1)+(RADIUS+1),
+					g:1 + Math.random(),
+					Vx:Math.pow(-1, Math.ceil(Math.random()*1000))*4,
+					Vy:-5,
+					color:"yellow"
+				}
+				balls.push(ball);
+			}
+		}
+	}
+}
 
 /*绘制剩余时间的图形*/
 function paintShape(x,y,num,context){
